a41c81103ca72d51c0bf2f0db8af52185d900d59
Oded Arbel <oded@geek.co.il>
2017-08-21 10:31:51 +0300

    first commit!

---
+++ README.md
@@ -0,0 +1,17 @@
+# The Simplest Vert.x-web Server
+
+This is a sample project to show the minimal deployment of a `vertx-web` based routing
+HTTP server.
+
+To run:
+
+```
+mvn org.codehaus.mojo:exec-maven-plugin:exec -Dexec.executable=java \
+	-Dexec.args="-cp %classpath io.vertx.core.Launcher run test.project1.Server"
+```
+
+To test:
+
+```
+curl -D- http://localhost:8080/
+```
---
+++ src/main/java/test/project1/Server.java
@@ -0,0 +1,33 @@
+package test.project1;
+
+import io.vertx.core.*;
+import io.vertx.core.http.*;
+import io.vertx.ext.web.*;
+
+public class Server extends AbstractVerticle {
+
+	private Router router;
+
+	@Override
+	public void start(Future<Void> fut) throws Exception {
+		router = Router.router(vertx);
+		router.route("/").handler(routingContext -> {
+			HttpServerResponse response = routingContext
+				.response();
+			response.putHeader("content-type", "text/html")
+				.end("<h1>Hello world</h1>");
+		});
+
+		vertx.createHttpServer().requestHandler(router::accept)
+			.listen(
+				config().getInteger("http.port", 8080),
+				result -> {
+					if (result.succeeded()) {
+						fut.complete();
+					} else {
+						fut.fail(result.cause());
+					}
+				});
+	}
+
+}
